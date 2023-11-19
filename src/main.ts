6ee6fe3903bfed19b90c0aba30e5e4fea79bf570
Omar Hedeya <omar.yasser.55@gmail.com>
2023-11-19 07:39:05 +0100

    remove code to start and stop docker

--- src/main.ts
+++ src/main.ts
@@ -662,34 +662,6 @@ export default class VCCopilotPlugin extends Plugin{
         this.status.setText('🧑‍🚀: VC Copilot loading....')
         this.status.setAttr('title', 'VC Copilot is loading...')
 
-        this.registerEvent(this.app.workspace.on('quit', () => {
-            this.stopContainers()
-        }))
-
-
-        exec(`export PATH=$PATH:${docker_path} && docker run -p 8080:8080 -d omarhedeya/vc_copilot_js:latest`, (error, stdout, stderr) => {
-            if(error){
-                console.error(`JS container creation error: ${error}`); 
-                new Notice('Could not run JS container'); 
-                return;
-            }
-            console.log(`stdout (JS creation of container): ${stdout}`)
-            console.log(`stderr (JS creation of container): ${stderr}`)
-            new Notice('JS container created')
-
-        })
-
-        exec(`export PATH=$PATH:${docker_path} && docker run -p 3030:3030 -d omarhedeya/vc_copilot_python:latest`, (error, stdout, stderr) => {
-            if(error){
-                console.error(`Python container creation error: ${error}`); 
-                new Notice('Could not run Python container'); 
-                return;
-            }
-            console.log(`stdout (python creation of container): ${stdout}`)
-            console.log(`stderr (python creation of container): ${stderr}`)
-            new Notice('Python container created')
-
-        })
 
         this.status.setText('🧑‍🚀: VC Copilot ready')
         this.status.setAttr('title', 'VC Copilot is ready')
@@ -701,42 +673,11 @@ export default class VCCopilotPlugin extends Plugin{
     onunload() {
         this.status.setText('🧑‍🚀: VC Copilot left')
         this.status.setAttr('title', 'VC Copilot says 👋')
-        /*exec(`export PATH=$PATH:${docker_path} && docker rm $(docker stop $(docker ps -a -q --filter ancestor=omar/expressjs:1.0))`, (error, stdout, stderr) => {
-            if(error){console.error(`JS container stop error\n${error}`); return;}
-            console.log(`stdout (JS stop container): ${stdout}`)
-            console.log(`stderr (JS stop container): ${stderr}`)
-            new Notice('JS Container stopped successfully')
-        })
 
-        exec(`export PATH=$PATH:${docker_path} && docker rm $(docker stop $(docker ps -a -q --filter ancestor=copilot/python_server:1.0))`, (error, stdout, stderr) => {
-            if(error){console.error(`Python container stop error\n${error}`); return;}
-            console.log(`stdout (Python stop container): ${stdout}`)
-            console.log(`stderr (Python stop container): ${stderr}`)
-            new Notice('Python Container stopped successfully')
-        })*/
-
-        this.stopContainers()
         
 
     }
 
-    stopContainers(){
-        exec(`export PATH=$PATH:${docker_path} && docker rm $(docker stop $(docker ps -a -q --filter ancestor=omarhedeya/vc_copilot_js:latest))`, (error, stdout, stderr) => {
-            if(error){console.error(`JS container stop error\n${error}`); return;}
-            console.log(`stdout (JS stop container): ${stdout}`)
-            console.log(`stderr (JS stop container): ${stderr}`)
-            new Notice('JS Container stopped successfully')
-        })
-
-        exec(`export PATH=$PATH:${docker_path} && docker rm $(docker stop $(docker ps -a -q --filter ancestor=omarhedeya/vc_copilot_python:latest))`, (error, stdout, stderr) => {
-            if(error){console.error(`Python container stop error\n${error}`); return;}
-            console.log(`stdout (Python stop container): ${stdout}`)
-            console.log(`stderr (Python stop container): ${stderr}`)
-            new Notice('Python Container stopped successfully')
-        })
-
-        
-    }
 
 
     async loadSettings(){
